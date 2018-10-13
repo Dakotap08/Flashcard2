f794b229684942404ede613fb3ccf6fcf9128e91
Dakota Price <dcwprice99@gmail.com>
2018-10-13 17:42:51 -0400

    Initial Commit

---
+++ Flashcard2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flashcard2
+//
+//  Created by Wine Mom Martha on 10/13/18.
+//  Copyright © 2018 Dakota Price. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
